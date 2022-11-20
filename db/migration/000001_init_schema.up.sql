676ed7c91a641e8f54519db4a8d63339707d8597
Ivan Markovski <ivan.markovski@scalefocus.com>
2022-11-20 22:03:52 +0100

    Initial API setup and SAML request

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -2,15 +2,16 @@ create table if not exists users
 (
     id         bigserial primary key,
     email      text unique,
-    country    text,
-    name       varchar(50),
     password   varchar(100) not null,
     last_login timestamp with time zone default CURRENT_TIMESTAMP
-                             );
 );
-alter table users
-    owner to root;
 
+create table if not exists user_preferences
+(
+    id         bigserial constraint user_preferences_pk primary key,
+    country    varchar(100),
+    user_id_fk bigserial constraint user_preferences_users_null_fk  references users
+);
 
 create table if not exists "ships-routes"
 (
