82cd9239b62d8a85ba99ebf7b4699f9de3e470a2
Ivan Markovski <ivan.markovski@scalefocus.com>
2023-05-09 23:22:14 +0200

    updated route controller, and google API's methods

--- api/pkg/controllers/route_controller.go
+++ api/pkg/controllers/route_controller.go
@@ -5,11 +5,12 @@ import (
 	"cadet-project/pkg/config"
 	"cadet-project/pkg/controllers/helper"
 	"cadet-project/pkg/interfaces"
+	"cadet-project/pkg/models"
 	"cadet-project/pkg/responses"
-	"cadet-project/pkg/utils"
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"googlemaps.github.io/maps"
 )
@@ -55,20 +56,20 @@ func (r *RouteController) GetDirections() ([]maps.Route, []google_API.GasStation
 
 	clientRequest.Origin, err = r.IShipPortsRepository.GetCityByName(r.Request.Context(), start)
 
-	origin := utils.CheckCity(clientRequest.Origin, start)
+	origin := CheckCity(clientRequest.Origin, start)
 	if origin == false {
 		return nil, nil, errors.New("point of origin do not exist in database")
 	}
 
 	clientRequest.Destination, err = r.IShipPortsRepository.GetCityByName(r.Request.Context(), end)
-	destination := utils.CheckCity(clientRequest.Destination, end)
+	destination := CheckCity(clientRequest.Destination, end)
 	if destination == false {
 		return nil, nil, errors.New("destination do not exist in database")
 	}
 
 	client := google_API.NewClientRequest(clientRequest.Origin, clientRequest.Destination)
 	vehicles, err := r.IUserVehicleRepository.FindVehiclesForUser(r.Request.Context(), UserID)
-	mileage := utils.MaxMileage(vehicles)
+	mileage := MaxMileage(vehicles)
 	route, gasStations, err := client.FindRoute(clientRequest, float64(mileage))
 	if err != nil {
 		return nil, nil, err
@@ -76,4 +77,26 @@ func (r *RouteController) GetDirections() ([]maps.Route, []google_API.GasStation
 
 	responses.JSON(r.Writer, 200, fmt.Sprintf("%s%d", "Total stops: ", google_API.GetStops()))
 	return route, gasStations, nil
+}
+func CheckCity(clientRequest string, start string) bool {
+	var err error
+
+	start = strings.Title(strings.ToLower(start))
+	if err != nil || clientRequest == "" || clientRequest != start {
+		return false
+	}
+
+	return true
+}
+
+func MaxMileage(vehicles []*models.Vehicle) float32 {
+	max := vehicles[0].Mileage
+
+	for _, m := range vehicles {
+		if max < m.Mileage {
+			max = m.Mileage
+		}
+	}
+
+	return max
 }
--- api/pkg/utils/utils.go
+++ api/pkg/utils/utils.go
@@ -1,34 +1,10 @@
 package utils
 
 import (
-	"cadet-project/pkg/models"
 	"html"
 	"strings"
 )
 
 func CleanUserData(str string) string {
 	return strings.TrimSpace(html.EscapeString(str))
-}
-
-func CheckCity(clientRequest string, start string) bool {
-	var err error
-
-	start = strings.Title(strings.ToLower(start))
-	if err != nil || clientRequest == "" || clientRequest != start {
-		return false
-	}
-
-	return true
-}
-
-func MaxMileage(vehicles []*models.Vehicle) float32 {
-	max := vehicles[0].Mileage
-
-	for _, m := range vehicles {
-		if max < m.Mileage {
-			max = m.Mileage
-		}
-	}
-
-	return max
 }
